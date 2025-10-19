41a6f8359d4eaa39d69a86452c5415ef4b832d60
ryckmob <oliveirachaves.ch97@gmail.com>
2025-10-19 20:43:39 -0300

    kkk

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,14 @@ export default function SocketPage(): JSX.Element {
   const [resp, setResp] = useState<string>('');
 
   useEffect(() => {
-    fetch('/api/socket');
-  
-    socket = io({
-      path: '/api/socket',
-    });
-  
-    socket.on('connect', () => {
-      console.log('Conectado ao servidor Socket.IO');
-    });
-  
-    socket.on('msg', (data: string) => {
-      setResp(data);
-    });
-  
+    socket = io({ path: '/api/socket' });
+
+    socket.on('msg', (data: string) => setResp(data));
+
     return () => {
       socket?.disconnect();
     };
   }, []);
-  
 
   const enviarMensagem = (): void => {
     if (!msg) return;
